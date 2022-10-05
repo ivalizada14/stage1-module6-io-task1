a79972fd579e1db4853fef6974b0523dadd2dae4
Ibrahim Velizade <ivalizada14@gmail.com>
2022-10-05 23:47:50 +0300

    first

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -10,7 +10,7 @@ public class FileReader {
     public Profile getDataFromFile(File file) {
         //FileInputStream fileInputStream = null;
         String fileContent = "";
-        try(FileInputStream fileInputStream = new FileInputStream("../source/Profile.txt");)
+        try(FileInputStream fileInputStream = new FileInputStream(file);)
         {
             int i = 0;
             while((i=fileInputStream.read())!=-1)
@@ -22,8 +22,14 @@ public class FileReader {
         {
             System.out.println(ex);
         }
+        System.out.println(fileContent);
         String parts[] = fileContent.split("\\s+");
-
+        System.out.println(parts[1]);
+        System.out.println(parts[3]);
+        System.out.println(parts[5]);
+        System.out.println(parts[7]);
         return new Profile(parts[1], Integer.parseInt(parts[3]), parts[5], Long.parseLong(parts[7]) );
+
     }
+
 }
